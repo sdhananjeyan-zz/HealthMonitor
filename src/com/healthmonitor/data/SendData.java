78ba5c09dd78ba7b7635e08661cdf70c4d533c6d
sdhananjeyan <sdhananjeyan@csc.com>
2018-04-12 17:46:42 +0530

    f

--- src/com/healthmonitor/data/SendData.java
+++ src/com/healthmonitor/data/SendData.java
@@ -3,6 +3,7 @@ package com.healthmonitor.data;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
@@ -10,9 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.SessionAware;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 import com.healthmonitor.hibernate.core.FactoryGenerator;
+import com.healthmonitor.pojo.RegisteredMachines;
 import com.healthmonitor.pojo.VMBasedData;
 import com.healthmonitor.util.Utils;
 import com.opensymphony.xwork2.ActionSupport;
@@ -27,25 +32,19 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 	private String date;
 	private String result = SUCCESS;
 	private String message = "";
+	private String ipAddress;
 
 	public String execute() {
 
 		try {
 			// String ipAddress = request.getRemoteAddr();
-			String ipAddress = "192.168.1.1";
 			activeUser = (String) session.get("user");
-			
+			//request.get
+
 			System.out.println(cpuStats);
 			System.out.println(memoryStats);
 			System.out.println(date);
 
-//			String data = "top - 18:03:04 up 8 days, 23:49,  2 users,  load average: 0.00, 0.00, 0.00\r\n"
-//					+ "Tasks:  78 total,   1 running,  77 sleeping,   0 stopped,   0 zombie\r\n"
-//					+ "Cpu(s):  0.0%us,  0.0%sy,  0.0%ni,100.0%id,  0.0%wa,  0.0%hi,  0.0%si,  0.0%st\r\n"
-//					+ "Mem:   1017060k total,   875564k used,   141496k free,   146388k buffers\r\n"
-//					+ "Swap:        0k total,        0k used,        0k free,   621376k cached";
-
-			
 			String cpuUtilU = cpuStats.split(":")[1].split(",")[0].split("%")[0];
 			String cpuUtilS = cpuStats.split(":")[1].split(",")[1].split("%")[0];
 			String cpuAvail = cpuStats.split(":")[1].split(",")[3].split("%")[0];
@@ -56,23 +55,23 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 
 			System.out.println(cpuUtilU + " " + cpuUtilS + " " + cpuAvail);
 			System.out.println(memToatl + " " + memUsed + " " + memAvail);
-			
-			if(cpuUtilU.contains(" ")) {
+
+			if (cpuUtilU.contains(" ")) {
 				cpuUtilU = cpuUtilU.split("( )+")[1];
 			}
-			if(cpuUtilS.contains(" ")) {
+			if (cpuUtilS.contains(" ")) {
 				cpuUtilS = cpuUtilS.split("( )+")[1];
 			}
-			if(cpuAvail.contains(" ")) {
+			if (cpuAvail.contains(" ")) {
 				cpuAvail = cpuAvail.split("( )+")[1];
 			}
-			if(memToatl.contains(" ")) {
+			if (memToatl.contains(" ")) {
 				memToatl = memToatl.split("( )+")[1];
 			}
-			if(memUsed.contains(" ")) {
+			if (memUsed.contains(" ")) {
 				memUsed = memUsed.split("( )+")[1];
 			}
-			if(memAvail.contains(" ")) {
+			if (memAvail.contains(" ")) {
 				memAvail = memAvail.split("( )+")[1];
 			}
 
@@ -80,14 +79,25 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 			vmBasedData.setIpAddress(ipAddress);
 			vmBasedData.setMemory((Double.parseDouble(memUsed) / Double.parseDouble(memToatl)) * 100);
 			vmBasedData.setProcess(((Double.parseDouble(cpuUtilU) + Double.parseDouble(cpuUtilS))
-					/ (Double.parseDouble(cpuUtilU) + Double.parseDouble(cpuUtilS) + Double.parseDouble(cpuAvail))) * 100);
+					/ (Double.parseDouble(cpuUtilU) + Double.parseDouble(cpuUtilS) + Double.parseDouble(cpuAvail)))
+					* 100);
 			vmBasedData.setTime(toDate(date));
 			Session hbSession = FactoryGenerator.sessionFactory.openSession();
 
 			try {
 				hbSession.beginTransaction();
-				hbSession.save(vmBasedData);
-				hbSession.getTransaction().commit();
+				Criteria criteria = hbSession.createCriteria(RegisteredMachines.class)
+						.add(Restrictions.eq("ipAddress", ipAddress));
+				List<VMBasedData> vms = criteria.list();
+				if (vms != null && vms.size() != 1) {
+					result = "failure";
+					message = "vm " + ipAddress + " not registered";
+
+				} else {
+					hbSession.save(vmBasedData);
+					hbSession.getTransaction().commit();
+				}
+
 			} catch (Exception e) {
 				result = "failue";
 				message = e.getMessage();
@@ -104,7 +114,6 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 
 		return SUCCESS;
 	}
-	
 
 	@Override
 	public void setSession(Map<String, Object> arg0) {
@@ -116,9 +125,6 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 		return activeUser;
 	}
 
-	
-	
-
 	@Override
 	public void setServletRequest(HttpServletRequest arg0) {
 		request = arg0;
@@ -128,9 +134,9 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 	public String getDate() {
 		return date;
 	}
-	
+
 	public Date toDate(String date) {
-		String [] dateSplit = date.split(" ");
+		String[] dateSplit = date.split(" ");
 		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(dateSplit[4]));
 		calendar.set(Calendar.DAY_OF_WEEK, Utils.getWeek(dateSplit[0]));
 		calendar.set(Calendar.MONTH, Utils.getMonth(dateSplit[1]));
@@ -139,11 +145,10 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 		calendar.set(Calendar.MINUTE, Integer.parseInt(dateSplit[3].split(":")[1]));
 		calendar.set(Calendar.SECOND, Integer.parseInt(dateSplit[3].split(":")[2]));
 		calendar.set(Calendar.YEAR, Integer.parseInt(dateSplit[5]));
-		
+
 		return calendar.getTime();
 	}
 
-	
 	public void setRequest(HttpServletRequest request) {
 		this.request = request;
 	}
@@ -156,23 +161,21 @@ public class SendData extends ActionSupport implements SessionAware, ServletRequ
 		return message;
 	}
 
-
 	public void setDate(String date) {
 		this.date = date;
 	}
-
 
 	public void setCpuStats(String cpuStats) {
 		this.cpuStats = cpuStats;
 	}
 
-
 	public void setMemoryStats(String memoryStats) {
 		this.memoryStats = memoryStats;
 	}
-	
-	
-	
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
 	
 
 }
